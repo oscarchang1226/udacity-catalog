51ab5d487e462af0c16b1b9c48eadf18aa1dcfe9
Oscar <oscarchang1226@gmail.com>
2017-01-31 12:27:10 -0600

    add category and item visits table

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime
+from sqlalchemy import text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -14,6 +15,7 @@ class User(Base):
     name = Column(String(250), nullable=False)
     salt = Column(String(250), nullable=False)
     hash = Column(String(250), nullable=False)
+    registered_on = Column(DateTime, server_default=text("NOW()"))
     img_url = Column(String(250))
 
     @property
@@ -33,6 +35,8 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    created_on = Column(DateTime, server_default=text("NOW()"))
+    description = Column(Text)
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
     user = relationship(User)
 
@@ -43,7 +47,8 @@ class Category(Base):
         """
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "description": self.description
         }
 
 
@@ -52,6 +57,8 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    created_on = Column(DateTime, server_default=text("NOW()"))
+    description = Column(Text)
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
     user = relationship(User)
     category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
@@ -64,8 +71,44 @@ class Item(Base):
         """
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "description": self.description
         }
+
+
+class CategoryVisit(Base):
+    __tablename__ = "category_visit"
+
+    category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+    category = relationship(Category)
+    visits = Column(Integer, server_default=1)
+
+    @property
+    def serialize(self):
+        """
+            Return object in JSON format
+        """
+        return {
+            "visits": self.visits
+        }
+
+
+class ItemVisit(Base):
+    __tablename__ = "item_visit"
+
+    item_id = Column(Integer, ForeignKey("item.id"), nullable=False)
+    item = relationship(Item)
+    visits = Column(Integer, server_default=1)
+
+    @property
+    def serialize(self):
+        """
+            Return object in JSON format
+        """
+        return {
+            "visits": self.visits
+        }
+
 
 engine = create_engine("sqlite:///catalog.db")
 
